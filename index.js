13c31cc2ba0180997c9ffba34474c228bba01dcb
charoenlap <charoenlap88@gmail.com>
2023-03-16 11:33:54 +0700

    update api

--- index.js
+++ index.js
@@ -6,13 +6,32 @@ connect();
 app.use(bodyParser.urlencoded({
   extended:true
 }));
+app.get('/users/:id', (req, res) => {
+  const userId = req.params.id;
+  const request = new sql.Request();
+  request.input('PageNum', sql.Int, 1);
+  request.input('PageSize', sql.Int, 10);
+  request.input('UserIds', sql.NVarChar(sql.MAX), userId);
+  request.execute('UserSelect', (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error executing stored procedure');
+    }
+    if (result.recordset.length === 0) {
+      return res.status(404).send('User not found');
+    }
+    res.send(result.recordset[0]);
+  });
+});
 app.get('/users', (req, res) => {
   sql.connect(config, err => {
     if (err) {
       console.log(err);
       return res.status(500).send('Error connecting to database');
     }
-    const query = "SELECT * FROM [dbo].[user]";
+    const pageNum = req.query.pageNum || 1;
+    const pageSize = req.query.pageSize || 10;
+    const query = `EXEC [dbo].[UserSelect] @PageNum = ${pageNum}, @PageSize = ${pageSize}`;
     sql.query(query, (err, result) => {
       console.log(result);
       if (err) {
@@ -24,41 +43,58 @@ app.get('/users', (req, res) => {
     });
   });
 });
-app.post('/users', async (req, res) => {
-  try {
-    const inputs = req.body;
-    const pool = sql.connect(config);
-    const request = pool.request();
-    const output = { message: sql.NVarChar(50) };
-
-    for (const [key, value] of Object.entries(inputs)) {
-      request.input(key, sql.NVarChar(value.length), value);
+app.post('/users', (req, res) => {
+  const { usergroup_id, position_id, username, password, name, lastname, phone, sex, date_create } = req.body;
+  const values = [usergroup_id, position_id, username, password, name, lastname, phone, sex, date_create];
+  
+  let  pool =  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error connecting to database');
     }
-
-    request.output('message', sql.NVarChar(50));
-
-    const result = await request.execute('InsertUser');
-
-    const message = result.output.message;
-    const values = [usergroup_id, position_id, username, password, name, lastname, phone, sex, date_create];
-    res.status(200).json({
-      success: true,
-      message: message,
-      data: values
+  });
+  try {
+    let message = "";
+    pool.request()
+      .input('usergroup_id', sql.Int, usergroup_id)
+      .input('position_id', sql.Int, position_id)
+      .input('username', sql.NVarChar(15), username)
+      .input('password', sql.NVarChar(15), password)
+      .input('name', sql.NVarChar(20), name)
+      .input('lastname', sql.NVarChar(30), lastname)
+      .input('phone', sql.NVarChar(10), phone)
+      .input('sex', sql.NVarChar(2), sex)
+      .output('message', sql.NVarChar(50))
+      .execute('UserInsert', function(err, returnValue) {
+        if (err){
+          const errorResult = {
+            code: 'E0001',
+            message: err
+          };
+          res.status(500).json({
+            success: false,
+            error: errorResult
+          });
+        }
+        console.log(returnValue);
+        message = returnValue.output.message;
+        res.status(200).json({
+          success: true,
+          message: message,
+          data: values
+        });
     });
-  } catch (err) {
-    console.log(err);
-    const errorResult = {
-      code: 'E0001',
-      message: 'An error occurred while inserting data'
-    };
-    res.status(500).json({
-      success: false,
-      error: errorResult
-    });
+  } catch (error) {
+      const errorResult = {
+        code: 'E0001',
+        message: 'An error occurred while retrieving data'
+      };
+      res.status(500).json({
+        success: false,
+        error: errorResult
+      });
   }
 });
-
 app.put('/users/:id', (req, res) => {
   const { usergroup_id, position_id, username, password, name, lastname, phone, sex } = req.body;
   const { id } = req.params;
@@ -91,8 +127,6 @@ app.put('/users/:id', (req, res) => {
     });
   });
 });
-
-
 app.delete('/users/:id', (req, res) => {
   let  pool =  sql.connect(config, err => {
     if (err) {
