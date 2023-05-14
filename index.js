b6fb5408679487246c2c08f25a1a96e473a30ba1
obiwan-overflow <kittipopp@hotmail.co.th>
2023-05-14 19:14:08 +0700

    update

--- index.js
+++ index.js
@@ -2,7 +2,8 @@ const express = require('express');
 const { connect, sql,config } = require('./db');
 const cors = require('cors');
 const app = express();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const { MAX } = require('mssql');
 connect();
 app.use(cors());
 app.use(bodyParser.urlencoded({
@@ -1292,7 +1293,8 @@ app.get('/expenses/:id', async (req, res) => {
       .input('PageNum', sql.Int, 1)
       .input('PageSize', sql.Int, 10)
       .input('ExpenseIds', sql.NVarChar(sql.MAX), expenseId)
-      .execute('[dbo].[SelectExpense]');
+      // .execute('[dbo].[SelectExpense]');
+      .execute('[dbo].[SelectTEExpense]');
     
     if (result.recordset.length === 0) {
       return res.status(404).send('Expense not found');
@@ -1312,7 +1314,8 @@ app.get('/expenses', async (req, res) => {
     const result = await pool.request()
       .input('PageNum', sql.Int, pageNum)
       .input('PageSize', sql.Int, pageSize)
-      .execute('[dbo].[SelectExpense]');
+      // .execute('[dbo].[SelectExpense]');
+      .execute('[dbo].[SelectTEExpense]');
     
     res.send(result.recordset);
   } catch (err) {
@@ -1321,14 +1324,14 @@ app.get('/expenses', async (req, res) => {
   }
 });
 app.post('/expenses', async (req, res) => {
-  const { expense_type_id, flag_draft, claim_no, claim_date, initials, amount, km, allowance, status, del, expense_by_user_id, expense_for_user_id, cost_center_user_id, finance_status, account_status } = req.body;
-  const values = [expense_type_id, flag_draft, claim_no, claim_date, initials, amount, km, allowance, status, del, expense_by_user_id, expense_for_user_id, cost_center_user_id, finance_status, account_status];
+  const { te_expense_type_id, flag_draft, claim_no, claim_date, initials, amount, km, allowance, status, del, te_expense_by_user_id, te_expense_for_user_id, cost_center_user_id, finance_status, account_status, date_add, expense_type } = req.body;
+  const values = [te_expense_type_id, flag_draft, claim_no, claim_date, initials, amount, km, allowance, status, del, te_expense_by_user_id, te_expense_for_user_id, cost_center_user_id, finance_status, account_status, date_add, expense_type];
   
   try {
     const pool = await sql.connect(config);
     let message = "";
     const result = await pool.request()
-      .input('expense_type_id', sql.Int, expense_type_id)
+      .input('te_expense_type_id', sql.Int, te_expense_type_id)
       .input('flag_draft', sql.Int, flag_draft)
       .input('claim_no', sql.NVarChar(10), claim_no)
       .input('claim_date', sql.DateTime, claim_date)
@@ -1338,11 +1341,13 @@ app.post('/expenses', async (req, res) => {
       .input('allowance', sql.Float(53), allowance)
       .input('status', sql.Int, status)
       .input('del', sql.Int, del)
-      .input('expense_by_user_id', sql.Int, expense_by_user_id)
-      .input('expense_for_user_id', sql.Int, expense_for_user_id)
+      .input('te_expense_by_user_id', sql.Int, te_expense_by_user_id)
+      .input('te_expense_for_user_id', sql.Int, te_expense_for_user_id)
       .input('cost_center_user_id', sql.Int, cost_center_user_id)
       .input('finance_status', sql.Int, finance_status)
       .input('account_status', sql.Int, account_status)
+      .input('date_add', sql.DateTime, date_add)
+      .input('expense_type', sql.NVarChar(10), expense_type)
       .output('message', sql.NVarChar(50))
       .execute('[dbo].[AddExpense]');
     
@@ -1473,38 +1478,66 @@ app.get('/expenses_ees/:id', async (req, res) => {
       return res.status(404).send('Expense not found');
     }
     
-    res.send(result.recordset[0]);
+    // res.send(result.recordset[0]);
+    res.send(result.recordset);
   } catch (err) {
     console.log(err);
     return res.status(500).send('Error executing stored procedure');
   }
 });
 app.post('/expenses_ees', async (req, res) => {
-  const { te_expense_id, date, type, invoice, descreption, amount, vat, wht, total_amount, total_base_vat, total_vat, total_withholding_tax, total_withholding_tax_big_absorb, total_expense, date_create } = req.body;
-  const values = [te_expense_id, date, type, invoice, descreption, amount, vat, wht, total_amount, total_base_vat, total_vat, total_withholding_tax, total_withholding_tax_big_absorb, total_expense, date_create];
+  const { te_expense_id, date, cost_center, expense_type, gl_account, sales_org, dc, plant, product, ship_to, work_order_no, wbs_no, network, activity, invoice_date, invoice, vat, total_amount, wht, amount_before_vat, vat_total, big_absorb_wht, amount_withholding_tax, descriptions, additional_notes, date_create } = req.body;
+  const values = [te_expense_id, date, cost_center, expense_type, gl_account, sales_org, dc, plant, product, ship_to, work_order_no, wbs_no, network, activity, invoice_date, invoice, vat, total_amount, wht, amount_before_vat, vat_total, big_absorb_wht, amount_withholding_tax, descriptions, additional_notes, date_create];
 
   
   try {
     const pool = await sql.connect(config);
     let message = "";
     const result = await pool.request()
+      // .input('te_expense_id', sql.Int, te_expense_id)
+      // .input('date', sql.DateTime, date)
+      // .input('type', sql.NVarChar(15), type)
+      // .input('invoice', sql.NVarChar(15), invoice)
+      // .input('descreption', sql.NVarChar(sql.MAX), descreption)
+      // .input('amount', sql.Float(53), amount)
+      // .input('vat', sql.Float(53), vat)
+      // .input('wht', sql.Float(53), wht)
+      // .input('total_amount', sql.Float(53), total_amount)
+      // .input('total_base_vat', sql.Float(53), total_base_vat)
+      // .input('total_vat', sql.Float(53), total_vat)
+      // .input('total_withholding_tax', sql.Float(53), total_withholding_tax)
+      // .input('total_withholding_tax_big_absorb', sql.Float(53), total_withholding_tax_big_absorb)
+      // .input('total_expense', sql.Float(53), total_expense)
+      // .input('date_create', sql.DateTime, date_create)
       .input('te_expense_id', sql.Int, te_expense_id)
       .input('date', sql.DateTime, date)
-      .input('type', sql.NVarChar(15), type)
-      .input('invoice', sql.NVarChar(15), invoice)
-      .input('descreption', sql.NVarChar(sql.MAX), descreption)
-      .input('amount', sql.Float(53), amount)
-      .input('vat', sql.Float(53), vat)
-      .input('wht', sql.Float(53), wht)
-      .input('total_amount', sql.Float(53), total_amount)
-      .input('total_base_vat', sql.Float(53), total_base_vat)
-      .input('total_vat', sql.Float(53), total_vat)
-      .input('total_withholding_tax', sql.Float(53), total_withholding_tax)
-      .input('total_withholding_tax_big_absorb', sql.Float(53), total_withholding_tax_big_absorb)
-      .input('total_expense', sql.Float(53), total_expense)
+      .input('cost_center', sql.VarChar(255), cost_center)
+      .input('expense_type', sql.VarChar(255), expense_type)
+      .input('gl_account', sql.VarChar(255), gl_account)
+      .input('sales_org', sql.VarChar(255), sales_org)
+      .input('dc', sql.VarChar(255), dc)
+      .input('plant', sql.VarChar(255), plant)
+      .input('product', sql.VarChar(255), product)
+      .input('ship_to', sql.VarChar(255), ship_to)
+      .input('work_order_no', sql.VarChar(15), work_order_no)
+      .input('wbs_no', sql.VarChar(15), wbs_no)
+      .input('network', sql.VarChar(255), network)
+      .input('activity', sql.VarChar(255), activity)
+      .input('invoice_date', sql.DateTime, invoice_date)
+      .input('invoice', sql.VarChar(15), invoice)
+      .input('vat', sql.VarChar(15), vat)
+      .input('total_amount', sql.VarChar(15), total_amount)
+      .input('wht', sql.VarChar(15), wht)
+      .input('amount_before_vat', sql.VarChar(15), amount_before_vat)
+      .input('vat_total', sql.VarChar(15), vat_total)
+      .input('big_absorb_wht', sql.VarChar(15), big_absorb_wht)
+      .input('amount_withholding_tax', sql.VarChar(15), amount_withholding_tax)
+      .input('descriptions', sql.VarChar(MAX), descriptions)
+      .input('additional_notes', sql.VarChar(MAX), additional_notes)
       .input('date_create', sql.DateTime, date_create)
       .output('message', sql.NVarChar(50))
-      .execute('[dbo].[AddTEExpenseEES]');
+      // .execute('[dbo].[AddTEExpenseEES]');
+      .execute('[dbo].[AddExpenseEes]');
     
     message = result.output.message;
     res.status(200).json({
@@ -1780,10 +1813,13 @@ app.post('/expenses_log', async (req, res) => {
     const pool = await sql.connect(config);
     let message = "";
     const result = await pool.request()
+    .input('te_expense_id', sql.Int, te_expense_id)
     .input('note', sql.NVarChar(100), note)
     .input('date_create', sql.DateTime, date_create)
-    .output('te_expense_log_id', sql.Int)
-    .execute('[dbo].[AddTEExpenseLog]');
+    // .output('te_expense_log_id', sql.Int)
+    // .execute('[dbo].[AddTEExpenseLog]');
+    .output('message',sql.NVarChar(50))
+    .execute('[dbo].[AddExpenseLog]');
     
     message = result.output.message;
     res.status(200).json({
@@ -1919,7 +1955,8 @@ app.post('/expenses_process', (req, res) => {
       .input('account_approve', sql.Int, account_approve)
       .input('hc_approve', sql.Int, hc_approve)
       .output('message', sql.NVarChar(50))
-      .execute('AddTEExpenseProcess', function(err, returnValue) {
+      // .execute('AddTEExpenseProcess', function(err, returnValue) {
+      .execute('AddExpenseProcess', function(err, returnValue) {
         if (err){
           const errorResult = {
             code: 'E0001',
@@ -2019,12 +2056,13 @@ app.delete('/expenses_process/:id', (req, res) => {
 });
 
 app.get('/expenses_travelling/:id', (req, res) => {
-  const userId = req.params.id;
+  const ExpenseTravellingIds = req.params.id;
   const request = new sql.Request();
   request.input('PageNum', sql.Int, 1);
   request.input('PageSize', sql.Int, 10);
-  request.input('UserIds', sql.NVarChar(sql.MAX), userId);
-  request.execute('SelectTEExpenseTravelling', (err, result) => {
+  request.input('ExpenseTravellingIds', sql.NVarChar(sql.MAX), ExpenseTravellingIds);
+  // request.execute('SelectTEExpenseTravelling', (err, result) => {
+  request.execute('SelectExpenseTravelling', (err, result) => {
     if (err) {
       console.log(err);
       return res.status(500).send('Error executing stored procedure');
@@ -2032,7 +2070,8 @@ app.get('/expenses_travelling/:id', (req, res) => {
     if (result.recordset.length === 0) {
       return res.status(404).send('TEExpenseTravelling not found');
     }
-    res.send(result.recordset[0]);
+    // res.send(result.recordset[0]);
+    res.send(result.recordset);
   });
 });
 app.get('/expenses_travelling', (req, res) => {
@@ -2056,8 +2095,8 @@ app.get('/expenses_travelling', (req, res) => {
   });
 });
 app.post('/expenses_travelling', (req, res) => {
-  const { te_expense_id, date, objectives, travelling_form, travelling_to, km, cost_all_days } = req.body;
-  const values = [te_expense_id, date, objectives, travelling_form, travelling_to, km, cost_all_days];
+  const { te_expense_id, date, objectives, travelling_form, travelling_to, km, cost_all_days, claim, round_trip, unit_rate, request_perdium, total_perdium, date_start, date_end } = req.body;
+  const values = [te_expense_id, date, objectives, travelling_form, travelling_to, km, cost_all_days, claim, round_trip, unit_rate, request_perdium, total_perdium, date_start, date_end];
   
   let  pool =  sql.connect(config, err => {
     if (err) {
@@ -2075,8 +2114,17 @@ app.post('/expenses_travelling', (req, res) => {
         .input('travelling_to', sql.NVarChar(15), travelling_to)
         .input('km', sql.Float(53), km)
         .input('cost_all_days', sql.Float(53), cost_all_days)
-        .output('te_expense_travelling_id', sql.Int)
-        .execute('[dbo].[AddTEExpenseTravelling]');
+        .input('claim', sql.NVarChar(10), claim)
+        .input('round_trip', sql.NVarChar(10), round_trip)
+        .input('unit_rate', sql.NVarChar(10), unit_rate)
+        .input('request_perdium', sql.NVarChar(10), request_perdium)
+        .input('total_perdium', sql.NVarChar(10), total_perdium)
+        .input('date_start', sql.DateTime, date_start)
+        .input('date_end', sql.DateTime, date_end)
+        // .output('te_expense_travelling_id', sql.Int)
+        // .execute('[dbo].[AddTEExpenseTravelling]');
+        .output('message', sql.NVarChar(50))
+        .execute('[dbo].[AddExpenseTravelling]');
         if (err){
           const errorResult = {
             code: 'E0001',
@@ -2494,7 +2542,7 @@ app.get('/news/:id', (req, res) => {
   request.input('PageNum', sql.Int, 1);
   request.input('PageSize', sql.Int, 10);
   request.input('NewsIds', sql.NVarChar(sql.MAX), userId);
-  request.execute('SelectUser', (err, result) => {
+  request.execute('SelectNews', (err, result) => {
     if (err) {
       console.log(err);
       return res.status(500).send('Error executing stored procedure');
@@ -2538,11 +2586,12 @@ app.post('/news', (req, res) => {
   try {
     let message = "";
     pool.request()
+      .input('news_title', sql.NVarChar(255), news_title)
       .input('news_detail', sql.NVarChar(sql.MAX), news_detail)
       .input('seq', sql.Int, seq)
       .input('del', sql.Int, del)
       .output('message', sql.NVarChar(50))
-      .execute('AddUser', function(err, returnValue) {
+      .execute('AddNews', function(err, returnValue) {
         if (err){
           const errorResult = {
             code: 'E0001',
@@ -2580,11 +2629,12 @@ app.put('/news/:id', (req, res) => {
   sql.connect(config).then(pool => {
     return pool.request()
       .input('news_id', sql.Int, id)
+      .input('news_title', sql.NVarChar(255), news_title)
       .input('news_detail', sql.NVarChar(sql.MAX), news_detail)
       .input('seq', sql.Int, seq)
       .input('del', sql.Int, del)
       .output('message', sql.NVarChar(50))
-      .execute('UpdateUser');
+      .execute('UpdateNews');
   }).then(result => {
     message = result.output.message;
     res.status(200).json({
@@ -3259,6 +3309,99 @@ app.delete('/userPosition/:id', (req, res) => {
         error: errorResult
       });
   }
+});
+
+// new flash
+// Gl Account
+app.get('/glAccount/:id', (req, res) => {
+  const expense_type = req.params.id;
+  const request = new sql.Request();
+  request.input('expense_type', sql.NVarChar(255), expense_type);
+  request.execute('SelectGlAccount', (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error executing stored procedure');
+    }
+    if (result.recordset.length === 0) {
+      return res.status(404).send('GL Account not found');
+    }
+    res.send(result.recordset);
+  });
+});
+
+// Cost Center
+app.get('/costCenter', (req, res) => {
+  const request = new sql.Request();
+  request.execute('SelectCostCenter', (err, result) => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error executing stored procedure');
+    }
+    if (result.recordset.length === 0) {
+      return res.status(404).send('GL Account not found');
+    }
+    res.send(result.recordset);
+  });
+});
+
+
+// select te 
+app.get('/selectTEprocessUser/:id', (req, res) =>{
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error connecting to database');
+    }
+    const id = req.params.id;
+    const query = "SELECT a.*,b.account_approve as status_account,b.hc_approve as status_hc FROM dbo.db_te_expense a LEFT JOIN dbo.db_te_expense_process b ON a.te_expense_id = b.te_expense_id";
+    sql.query(query, (err, result) => {
+      console.log(result);
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Error executing query');
+      }
+      res.send(result.recordset);
+    });
+  });
+});
+app.get('/selectTEprocess/:account_approve/:hc_approve', (req, res) =>{
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error connecting to database');
+    }
+    const account_approve = req.params.account_approve;
+    const hc_approve = req.params.hc_approve;
+    const query = "SELECT a.* FROM dbo.db_te_expense a LEFT JOIN dbo.db_te_expense_process b ON a.te_expense_id = b.te_expense_id WHERE a.flag_draft = 0 AND b.account_approve ="+account_approve+" AND b.hc_approve ="+hc_approve+"";
+    sql.query(query, (err, result) => {
+      console.log(result);
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Error executing query');
+      }
+      res.send(result.recordset);
+    });
+  });
+});
+app.get('/approveTEprocess/:id/:account_approve/:hc_approve', (req, res) =>{
+  sql.connect(config, err => {
+    if (err) {
+      console.log(err);
+      return res.status(500).send('Error connecting to database');
+    }
+    const te_expense_id = req.params.id;
+    const account_approve = req.params.account_approve;
+    const hc_approve = req.params.hc_approve;
+    const query = "UPDATE db_te_expense_process SET account_approve = "+account_approve+", hc_approve = "+hc_approve+" WHERE te_expense_id ="+te_expense_id+"";
+    sql.query(query, (err, result) => {
+      console.log(result);
+      if (err) {
+        console.log(err);
+        return res.status(500).send('Error executing query');
+      }
+      res.send(result.rowsAffected);
+    });
+  });
 });
 
 app.listen(3003, () => {
